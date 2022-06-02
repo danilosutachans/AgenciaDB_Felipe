5ea76606661411956b9909ba153516ec92c0bdcd
Danilo Sutachán Sánchez <danilosutachans@gmail.com>
2022-06-02 16:07:27 -0500

    Update index.php
    Update for fetch rows of Cities.

--- index.php
+++ index.php
@@ -15,10 +15,42 @@
 		<input type="text" name="cedula" placeholder="Numero Identificacion">
 		<input type="date" name="fechanac" placeholder="Fecha Nacimiento">
 		<input type="text" name="telefono" placeholder="Celular">
+		<br>
+		<br>
+
+		<?php
+		//Conexión BD
+		include ("con_db.php");
+		//Consultar ciudades
+		if($conex === false){
+			die("ERROR: Could not connect. " . mysqli_connect_error());
+		}
+		$sql = "SELECT Ciudad FROM Ciudades";
+		echo "<label for='CiudadOrigen'> Seleccione la ciudad de Origen: </label>";
+		echo "<select name='CiudadOrigen' id='CiudadOrigen'>";
+		if($result = mysqli_query($conex, $sql)){
+			if(mysqli_num_rows($result) > 0){
+				while($row = mysqli_fetch_array($result)){
+					echo "<option>" . $row['Ciudad'] . "</option>";
+				}
+		echo "</select>";
+		// Free result set
+		mysqli_free_result($result);
+		} else {
+				echo "No records matching your query were found.";
+			}
+		}
+		?>
+
+		<br>
+		<br>
+		<label for="CiudadDestino">Seleccione la ciudad de Destino:</label>
+		<select name="CiudadDestino" id="CiudadDestino">
+			<option value="Bogota">
+				BOGOTA
+			</option>
+		</select>
 		<input type="submit" name="register">
-	    <select name="select_ciudades">
-		    <option>BOGOTA</option>
-	    </select>
     </form>
         <?php 
         include("registrar.php");
